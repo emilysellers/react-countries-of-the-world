94766b3ace0a9bdf73742a7b141fac49f41951be
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-17 16:07:48 -0800

    set up component folders and files

---
+++ src/components/Main/Main.js
@@ -0,0 +1,8 @@
+export default function Main() {
+  return (
+    <main className="countryPage">
+      <h1>Countries of the world</h1>
+      {/* // <main>country cards</main> */}
+    </main>
+  );
+}
