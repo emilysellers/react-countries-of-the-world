4ef26312d1280e830484f3166aa0625c6220b3d5
Emily Sellers <emilymelindasellers@gmail.com>
2023-01-17 17:27:12 -0800

    add flagpedia API to load each country's flag

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,10 +1,11 @@
 import './Country.css';
 
-export default function Country({ name }) {
+export default function Country({ name, iso2 }) {
   return (
     <div>
       <div className="countryCard">
         <h2>{name}</h2>
+        <img src={`https://flagcdn.com/160x120/${iso2.toLowerCase()}.png`}></img>
       </div>
     </div>
   );
