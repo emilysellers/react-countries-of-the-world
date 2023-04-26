58ccf3b6ef1f5964390fd27d3e9e9ee053544127
Emily Sellers <emilymelindasellers@gmail.com>
2023-04-25 21:57:03 -0700

    Fix height so components fill page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Footer() {
-  return <p>developed by Emily Sellers 2023</p>;
+  return <p style={{ margin: 0, padding: '40px' }}>developed by Emily Sellers 2023</p>;
 }
